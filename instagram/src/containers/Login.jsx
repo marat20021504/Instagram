ffb1f13f60f0896ae7bd53ee71a6444f6e2c6f87
marat20021504 <maratqochqorov@gmail.com>
2022-04-04 18:07:38 +0500

    add Bar

--- instagram/src/containers/Login.jsx
+++ instagram/src/containers/Login.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+import Bar from '../components/Bar'
 
 export default function Login() {
   return (
-    <div>Login</div>
+      <Wrapper>
+        <Bar />
+      </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  
+`
